8eda23f9a84cbb349c93fb64b862bc31f5c0d505
AMIRAN <129764164+AMIRAN1122@users.noreply.github.com>
2023-05-04 15:42:44 +0330

    Update sed.py

--- sed.py
+++ sed.py
@@ -1,3 +1,37 @@
 import streamlit as st
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from PIL import Image
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
-st.write("A")
+if st.button(label="Start"):
+
+    service = Service(ChromeDriverManager().install())
+
+    options = webdriver.ChromeOptions()
+
+    options.add_argument("--headless")
+
+    options.add_argument("--disable-gpu")
+
+    # options.add_argument("--no-sandbox")
+
+    # options.add_argument("--disable-dev-shm-usage")
+
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36")
+
+    driver = webdriver.Chrome(service=service, options=options)
+
+    driver.get("https://snapinsta.app/")
+    
+    time.sleep(10)
+    
+    driver.save_screenshot("image.png")
+    
+    image = Image.open("image.png")
+    
+    st.image(image, caption='This is for you')
