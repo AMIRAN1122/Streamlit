dca43cfe267d33b5d53977bf7c2ce54e75fa2d11
AMIRAN <129764164+AMIRAN1122@users.noreply.github.com>
2023-05-04 20:44:34 +0330

    Update sed.py

--- sed.py
+++ sed.py
@@ -43,6 +43,7 @@ if st.button(label="Start"):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36")
 
     driver = webdriver.Remote(
         command_executor='https://red.iran.liara.run/',
