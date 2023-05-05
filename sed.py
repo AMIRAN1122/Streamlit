3a3c660f18f7c18bcd537fb2757ad246e66423e4
AMIRAN <129764164+AMIRAN1122@users.noreply.github.com>
2023-05-05 14:09:18 +0330

    Update sed.py

--- sed.py
+++ sed.py
@@ -41,12 +41,8 @@ if st.button(label="Start"):
     chrome_capabilities['browserless.token'] = 'WR2t4sKXbSX0h'
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--port=80')
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36")
-    
+  
     driver = webdriver.Remote(
         command_executor='https://chrome-pyjbqmuda.iran.liara.run/',
         desired_capabilities=chrome_capabilities,
