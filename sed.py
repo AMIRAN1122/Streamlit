0102b01781abbc980cbb85864a00098597158c4d
AMIRAN <129764164+AMIRAN1122@users.noreply.github.com>
2023-05-07 21:16:42 +0330

    Update sed.py

--- sed.py
+++ sed.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+
 if st.button(label="Start"):
 
     chrome_options = webdriver.ChromeOptions()
