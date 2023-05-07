aae04102219044142662e02448d23c6b5d615a77
AMIRAN <129764164+AMIRAN1122@users.noreply.github.com>
2023-05-07 22:55:57 +0330

    Update sed.py

--- sed.py
+++ sed.py
@@ -8,38 +8,40 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-html_string = """
-<script>
-            !function(e,t,n){e.yektanetAnalyticsObject=n,e[n]=e[n]||function(){e[n].q.push(arguments)},e[n].q=e[n].q||[];var a=t.getElementsByTagName("head")[0],r=new Date,c="https://cdn.yektanet.com/superscript/QOqTe3Bm/native-amiran1122-red-red-lkrpex.streamlit.app-31446/yn_pub.js?v="+r.getFullYear().toString()+"0"+r.getMonth()+"0"+r.getDate()+"0"+r.getHours(),s=t.createElement("link");s.rel="preload",s.as="script",s.href=c,a.appendChild(s);var l=t.createElement("script");l.async=!0,l.src=c,a.appendChild(l)}(window,document,"yektanet");
-        </script>
+st.write("Hello world")
 
-<div id="pos-article-display-82621"></div>     
-"""
+# html_string = """
+# <script>
+#             !function(e,t,n){e.yektanetAnalyticsObject=n,e[n]=e[n]||function(){e[n].q.push(arguments)},e[n].q=e[n].q||[];var a=t.getElementsByTagName("head")[0],r=new Date,c="https://cdn.yektanet.com/superscript/QOqTe3Bm/native-amiran1122-red-red-lkrpex.streamlit.app-31446/yn_pub.js?v="+r.getFullYear().toString()+"0"+r.getMonth()+"0"+r.getDate()+"0"+r.getHours(),s=t.createElement("link");s.rel="preload",s.as="script",s.href=c,a.appendChild(s);var l=t.createElement("script");l.async=!0,l.src=c,a.appendChild(l)}(window,document,"yektanet");
+#         </script>
 
-st.markdown(html_string, unsafe_allow_html=True)
+# <div id="pos-article-display-82621"></div>     
+# """
 
-if st.button(label="Start"):
+# st.markdown(html_string, unsafe_allow_html=True)
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.set_capability('browserless:token', 'SDflkgfhlslktydskhga')
+# if st.button(label="Start"):
 
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--headless")
+#     chrome_options = webdriver.ChromeOptions()
+#     chrome_options.set_capability('browserless:token', 'SDflkgfhlslktydskhga')
 
-    driver = webdriver.Remote(
-        command_executor='https://s-chrome.iran.liara.run/webdriver',
-        options=chrome_options
-    )
+#     chrome_options.add_argument("--no-sandbox")
+#     chrome_options.add_argument("--headless")
 
-    driver.get("https://streamlit.iran.liara.run/")
+#     driver = webdriver.Remote(
+#         command_executor='https://s-chrome.iran.liara.run/webdriver',
+#         options=chrome_options
+#     )
+
+#     driver.get("https://streamlit.iran.liara.run/")
     
-    page_source = driver.page_source
+#     page_source = driver.page_source
     
-    st.write("1 2 3 OK")
+#     st.write("1 2 3 OK")
     
-    driver.quit()
+#     driver.quit()
     
-    st.write(page_source)
+#     st.write(page_source)
 
     
 #     try:
