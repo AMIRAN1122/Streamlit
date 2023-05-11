d9c14cbdaefae31434f5b1a7d58a5ffbf54f0b63
AMIRAN <129764164+AMIRAN1122@users.noreply.github.com>
2023-05-11 03:37:05 +0330

    Update home.py

--- home.py
+++ home.py
@@ -3,25 +3,15 @@ import streamlit.components.v1 as components
 
 st.write("Hello world")
 
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.write("VVV")
-    
-with col2:
-    st.write("AAAAAAAA")
-    components.html(
-        """
-        <head>
-        <script>
-                !function(e,t,n){e.yektanetAnalyticsObject=n,e[n]=e[n]||function(){e[n].q.push(arguments)},e[n].q=e[n].q||[];var a=t.getElementsByTagName("head")[0],r=new Date,c="https://cdn.yektanet.com/superscript/UegHYjU6/native-streamlit.iran.liara.run-31555/yn_pub.js?v="+r.getFullYear().toString()+"0"+r.getMonth()+"0"+r.getDate()+"0"+r.getHours(),s=t.createElement("link");s.rel="preload",s.as="script",s.href=c,a.appendChild(s);var l=t.createElement("script");l.async=!0,l.src=c,a.appendChild(l)}(window,document,"yektanet");
-            </script>
-        <div id="pos-article-display-82887"></div>
-        </head> 
-        """,
-        height=625,
-        width=625,
-    )
-    
-with col3:
-    st.write("VVV")
+components.html(
+    """
+    <head>
+    <script>
+            !function(e,t,n){e.yektanetAnalyticsObject=n,e[n]=e[n]||function(){e[n].q.push(arguments)},e[n].q=e[n].q||[];var a=t.getElementsByTagName("head")[0],r=new Date,c="https://cdn.yektanet.com/superscript/UegHYjU6/native-streamlit.iran.liara.run-31555/yn_pub.js?v="+r.getFullYear().toString()+"0"+r.getMonth()+"0"+r.getDate()+"0"+r.getHours(),s=t.createElement("link");s.rel="preload",s.as="script",s.href=c,a.appendChild(s);var l=t.createElement("script");l.async=!0,l.src=c,a.appendChild(l)}(window,document,"yektanet");
+        </script>
+    <div id="pos-article-display-82887"></div>
+    </head> 
+    """,
+    height=1000,
+    width=625,
+)
