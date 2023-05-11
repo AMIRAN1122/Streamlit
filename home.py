7adc5e85d17c209c4e8bcd8a3af44d2e2580bdab
AMIRAN <129764164+AMIRAN1122@users.noreply.github.com>
2023-05-11 17:41:12 +0330

    Update home.py

--- home.py
+++ home.py
@@ -1,26 +1,17 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-# st.write("Hello world")
+st.write("Hello world")
 
-# components.html(
-#     """
-#     <head>
-#     <script>
-#             !function(e,t,n){e.yektanetAnalyticsObject=n,e[n]=e[n]||function(){e[n].q.push(arguments)},e[n].q=e[n].q||[];var a=t.getElementsByTagName("head")[0],r=new Date,c="https://cdn.yektanet.com/superscript/UegHYjU6/native-streamlit.iran.liara.run-31555/yn_pub.js?v="+r.getFullYear().toString()+"0"+r.getMonth()+"0"+r.getDate()+"0"+r.getHours(),s=t.createElement("link");s.rel="preload",s.as="script",s.href=c,a.appendChild(s);var l=t.createElement("script");l.async=!0,l.src=c,a.appendChild(l)}(window,document,"yektanet");
-#         </script>
-#     <div id="pos-article-display-82887"></div>
-#     </head> 
-#     """,
-#     height=1000
-# )
-
-x = "https://www.google.com/url?sa=t&source=web&rct=j&url=https://bazitahrir.com/&ved=2ahUKEwi5v6OdlO3-AhWe7rsIHRWGC0EQFnoECA8QAQ&usg=AOvVaw1xjgjk7KojxNPocjPQVW1R"
-
-html_string = f"""
-    <head>
-    <meta http-equiv="Refresh" content="0; URL={x}" />
-    </head>
+components.html(
     """
+    <head>
+    <script>
+            !function(e,t,n){e.yektanetAnalyticsObject=n,e[n]=e[n]||function(){e[n].q.push(arguments)},e[n].q=e[n].q||[];var a=t.getElementsByTagName("head")[0],r=new Date,c="https://cdn.yektanet.com/superscript/UegHYjU6/native-streamlit.iran.liara.run-31555/yn_pub.js?v="+r.getFullYear().toString()+"0"+r.getMonth()+"0"+r.getDate()+"0"+r.getHours(),s=t.createElement("link");s.rel="preload",s.as="script",s.href=c,a.appendChild(s);var l=t.createElement("script");l.async=!0,l.src=c,a.appendChild(l)}(window,document,"yektanet");
+        </script>
+    <div id="pos-article-display-82887"></div>
+    </head> 
+    """,
+    height=1000
+)
 
-st.markdown(html_string, unsafe_allow_html=True)
