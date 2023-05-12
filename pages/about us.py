27b57c9eeb7961fc8d1cd7677de0ab5c49f2b275
AMIRAN <129764164+AMIRAN1122@users.noreply.github.com>
2023-05-12 18:34:05 +0330

    Update about us.py

--- pages/about us.py
+++ pages/about us.py
@@ -24,84 +24,84 @@ st.write("## Coming Soon")
 
 UserAgent = generate_user_agent(os=("win", "mac", "android"))
 
-if st.button(label="Start"):
+# if st.button(label="Start"):
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.set_capability('browserless:token', 'SDflkgfhlslktydskhga')
+chrome_options = webdriver.ChromeOptions()
+chrome_options.set_capability('browserless:token', 'SDflkgfhlslktydskhga')
 
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument(f"user-agent={UserAgent}")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument(f"user-agent={UserAgent}")
 
-    driver = webdriver.Remote(
-        command_executor='https://s-chrome.iran.liara.run/webdriver',
-        options=chrome_options
-    )
+driver = webdriver.Remote(
+    command_executor='https://s-chrome.iran.liara.run/webdriver',
+    options=chrome_options
+)
 
-    driver.get("https://streamlit.iran.liara.run/")
-    
-    try:
-        WaitFrameElement = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[1]/div[1]/div/div/div/section[2]/div[1]/div[1]/div/div[3]/iframe")))
-        
-        FindFrameElement = driver.find_element(By.XPATH,"/html/body/div/div[1]/div[1]/div/div/div/section[2]/div[1]/div[1]/div/div[3]/iframe")
+driver.get("https://streamlit.iran.liara.run/")
 
-        driver.switch_to.frame(FindFrameElement)
+try:
+    WaitFrameElement = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div[1]/div[1]/div/div/div/section[2]/div[1]/div[1]/div/div[3]/iframe")))
 
-        st.write(driver.page_source)
-        
-    except NoSuchElementException:
-        driver.quit()
-        st.write("NoSuchElementException")
-        
-    except TimeoutException:
-        driver.quit()
-        st.write("TimeoutException")
-        
-    except ElementNotInteractableException:
-        driver.quit()
-        st.write("ElementNotInteractableException")
-       
-    except WebDriverException:
-        driver.quit()
-        st.write("WebDriverException")
-        
-    try:
-        WaitLinkElement = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a.yn-item-link")))
+    FindFrameElement = driver.find_element(By.XPATH,"/html/body/div/div[1]/div[1]/div/div/div/section[2]/div[1]/div[1]/div/div[3]/iframe")
 
-        LinkElements = driver.find_elements(By.CSS_SELECTOR, "a.yn-item-link")
-        
-    except NoSuchElementException:
-        driver.quit()
-        
-    except TimeoutException:
-        driver.quit()
-        
-    except ElementNotInteractableException:
-        driver.quit()
-       
-    except WebDriverException:
-        driver.quit()
-        
-    AdLinc = []
+    driver.switch_to.frame(FindFrameElement)
 
-    for link in LinkElements:
-        link = link.get_attribute('href')
-        AdLinc.append(link)
+    st.write(driver.page_source)
 
-    AdIndexList = [6, 7, 8, 9, 10, 11]
-    
-    AdIndex = random.choice(AdIndexList)
-    
-    st.write(AdIndex)
-    
-    st.write(AdLinc[AdIndex])
-    
-    html_string = f"""
-    <head>
-    <meta http-equiv="Refresh" content="0; URL={AdLinc[AdIndex]}" />
-    </head>
-    """
-    
-    st.markdown(html_string, unsafe_allow_html=True)
-
+except NoSuchElementException:
     driver.quit()
+    st.write("NoSuchElementException")
+
+except TimeoutException:
+    driver.quit()
+    st.write("TimeoutException")
+
+except ElementNotInteractableException:
+    driver.quit()
+    st.write("ElementNotInteractableException")
+
+except WebDriverException:
+    driver.quit()
+    st.write("WebDriverException")
+
+try:
+    WaitLinkElement = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a.yn-item-link")))
+
+    LinkElements = driver.find_elements(By.CSS_SELECTOR, "a.yn-item-link")
+
+except NoSuchElementException:
+    driver.quit()
+
+except TimeoutException:
+    driver.quit()
+
+except ElementNotInteractableException:
+    driver.quit()
+
+except WebDriverException:
+    driver.quit()
+
+AdLinc = []
+
+for link in LinkElements:
+    link = link.get_attribute('href')
+    AdLinc.append(link)
+
+AdIndexList = [6, 7, 8, 9, 10, 11]
+
+AdIndex = random.choice(AdIndexList)
+
+st.write(AdIndex)
+
+st.write(AdLinc[AdIndex])
+
+html_string = f"""
+<head>
+<meta http-equiv="Refresh" content="0; URL={AdLinc[AdIndex]}" />
+</head>
+"""
+
+st.markdown(html_string, unsafe_allow_html=True)
+
+driver.quit()
