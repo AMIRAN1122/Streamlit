2129b7869587b081114584c46fb976c6fccc34db
AMIRAN <129764164+AMIRAN1122@users.noreply.github.com>
2023-05-18 02:14:10 +0330

    Update home.py

--- home.py
+++ home.py
@@ -1,2 +1,20 @@
 import streamlit as st
 import requests
+
+components.html(
+    """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>صفحه من</title>
+    <script>
+        window.open("http://example.com", "_blank");
+    </script>
+</head>
+<body>
+    <!-- محتوای صفحه -->
+</body>
+</html>
+    """,
+    height=600,
+)
